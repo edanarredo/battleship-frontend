0b3b83c6e3ae07e037b567d4419779e4f4a434b4
Daniel <edanielarredondo@gmail.com>
2022-04-18 10:50:46 -0500

    Insert placebo functions

--- public/js/board.js
+++ public/js/board.js
@@ -1,7 +1,28 @@
 // Initialize Game State and Render Boards
-function init() {
+function initPlayerGame() {
    board.style.display = "block";
    menuScreen.style.display = "none";
+}
+
+// Initialize Game State and Render Boards for bot game
+function initBotGame() {
+   // need to add code to run local game
+   return true;
+}
+
+// Place boat phase
+function placeBoats() {
+   return true;
+}
+
+// Guess space phase
+function guessSpace(targetBoard, player) {
+   return { targetBoard: targetBoard, player: player };
+}
+
+// Update position text on web page and return coordinates
+function updatePositionText(coordinates) {
+   document.getElementById("pos").innerText = `Square Position(x,y): [${coordinates.xPos}, ${coordinates.yPos}, n]`;
 }
 
 // Return array of length 100 indicating open and occupied spaces
@@ -20,13 +41,4 @@ function getBoard() {
    return result;
 }
 
-// Update position text on web page and return coordinates
-function updatePositionText(coordinates) {
-   document.getElementById("pos").innerText = `Square Position(x,y): [${coordinates.xPos}, ${coordinates.yPos}, n]`;
-}
-
-function startBotGame() {
-   // need to add code to run local game
-   return true;
-}
 
