856d143ca4e04db9b8b5ac79fc7ea4836169cda0
Daniel <edanielarredondo@gmail.com>
2022-05-04 06:40:26 -0500

    Add win message for singleplayer

--- public/js/bot.js
+++ public/js/bot.js
@@ -22,8 +22,12 @@ var all_ship_statuses = {
 };
 
 
-function botGuess() {
+async function botGuess() {
+
+   gameStatus.innerText = "Opponent's turn.";
    while (!usersTurn) {
+
+      await new Promise(r => setTimeout(r, 1000));
 
       // Draw a random number from botGuesses[]
       var index = Math.floor(Math.random() * (botGuesses.length + 1));
@@ -53,20 +57,20 @@ function botGuess() {
                all_ship_statuses.user['S']--;
                opponentPoints++;
                break;
-            // Missed guess
             default:
-               usersTurn = true;
                break;
          }
 
          // Leave marker indicating hit.
-         boxes[index].innerText = "-1";
+         boxes[index].innerHTML = `<div style="border: 4px solid RED !important; height: 100%; width: 100%;">HIT</div>`;
+         userBoard[index] = 0;
       }
 
       // If a miss, leave marker indicating miss.
       else if (targetValue == 0) {
-         boxes[index].innerText = "0";
+         boxes[index].innerHTML = `<div style="border: 4px solid YELLOW !important; height: 100%; width: 100%;">MISS</div>`;
          usersTurn = true;
+         gameStatus.innerText = "Your turn!";
       } 
 
 
